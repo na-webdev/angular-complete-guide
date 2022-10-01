9203c60d437308abecac76305a27ff85264824d6
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-10-01 16:24:40 +0500

    reactive forms finished

---
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewChild } from '@angular/core'
+import { NgForm } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm
+  onSubmit() {
+    console.log(this.signUpForm.value)
+  }
+}
--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -7,19 +7,50 @@ import { NgForm } from '@angular/forms'
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('f') form: NgForm
+  @ViewChild('f') signUpForm: NgForm
+  answer = ''
+  genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    gender: '',
+    secretQuestion: '',
+    answer: '',
+  }
+  submitted = false
   suggestUserName() {
     const suggestedName = 'Superuser'
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: 'dog',
+    //   gender: 'male',
+    // })
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    })
   }
 
   onSubmit() {
-    console.log(this.form)
-    console.log(this.form.value, 'VALUE')
-    console.log(this.form.touched, 'Touched')
-    console.log(this.form.dirty, 'Dirty')
-    console.log(this.form.submitted, 'Submitted')
-    console.log(this.form.controls, 'Controls')
-    console.log(this.form.control, 'Control')
-    console.log(this.form.valid, 'Valid')
+    console.log(this.signUpForm)
+    console.log(this.signUpForm.value, 'VALUE')
+    console.log(this.signUpForm.touched, 'Touched')
+    console.log(this.signUpForm.dirty, 'Dirty')
+    console.log(this.signUpForm.submitted, 'Submitted')
+    console.log(this.signUpForm.controls, 'Controls')
+    console.log(this.signUpForm.control, 'Control')
+    console.log(this.signUpForm.valid, 'Valid')
+    this.user.username = this.signUpForm.value.userData.username
+    this.user.email = this.signUpForm.value.userData.email
+    this.user.secretQuestion = this.signUpForm.value.secretQuestion
+    this.user.answer = this.signUpForm.value.questionAnswer
+    this.user.gender = this.signUpForm.value.gender
+    this.submitted = true
+    this.signUpForm.reset()
   }
 }
