1958fffacbc5aeb71bad9ff2a07e55c29c8c63f7
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-10-02 11:27:41 +0500

    reactive forms

---
+++ forms-reactive-start/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl('male'),
+      hobbies: new FormArray([]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm.value);
+  }
+
+  onAddHobby() {
+    const control = new FormControl('', Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  get controls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+}
--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms'
 })
 export class AppComponent {
   @ViewChild('f') signUpForm: NgForm
+
   onSubmit() {
     console.log(this.signUpForm.value)
   }
