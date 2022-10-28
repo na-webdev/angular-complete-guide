405a5f60756a2b941d6513f1c2f7fbb4a1691155
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-10-28 08:29:30 +0500

    directives in depth

--- angular_in_depth/src/app/app.module.ts
+++ angular_in_depth/src/app/app.module.ts
@@ -5,9 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
+import { CustomTwoWayComponent } from './components/data-binding/custom-two-way/custom-two-way.component';
+import { CounterComponent } from './components/data-binding/custom-two-way/counter.component';
+import { TtClassDirective } from './directives/tt-class.directive';
+import { CustomIfDirective } from './directives/custom-if.directive';
 
 @NgModule({
-  declarations: [AppComponent, DataBindingComponent],
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    CustomTwoWayComponent,
+    CounterComponent,
+    TtClassDirective,
+    CustomIfDirective,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
