ba09393e275c98077694f22a5a41a2320a503da5
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-11-08 11:25:27 +0500

    life cycle hooks

--- angular_in_depth/src/app/app.component.ts
+++ angular_in_depth/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular_in_depth';
+  message = 'Message';
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.message += ' ' + Math.floor(Math.random() * 10);
+    }, 2000);
+    setTimeout(() => (this.title = 'Angular'), 8000);
+  }
 }
